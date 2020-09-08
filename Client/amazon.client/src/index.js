ac7aea4319409b473b6a8adb893ae7957ba533b4
Atanas Atanasov <atanas4537@gmail.com>
2020-09-08 12:51:03 +0300

    add react router navigation and move App.js in src folder

--- Client/amazon.client/src/index.js
+++ Client/amazon.client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Navigator from "./navigation";
 import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<App>
+			<Navigator />
+		</App>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
