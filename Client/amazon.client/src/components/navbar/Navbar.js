8116a1d2db96cda7b814f5622585646bb688fa83
Atanas Atanasov <atanas4537@gmail.com>
2020-09-09 07:01:40 +0300

    finalize navbar component

--- Client/amazon.client/src/components/navbar/Navbar.js
+++ Client/amazon.client/src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { useGlobalContext } from "../../context/context";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import HistoryIcon from "@material-ui/icons/History";
 
 const Navbar = () => {
-	const [searchStyles, setSearchStyles] = useState(styles.search);
+	const { isLoggedIn, logout } = useGlobalContext();
 
 	return (
 		<nav className={styles.header}>
@@ -13,22 +17,51 @@ const Navbar = () => {
 				<img src="logo.png" className={styles.logo} alt="logo" />
 			</Link>
 
-			<div className={searchStyles}>
-				<input
-					type="text"
-					className={styles["search-input"]}
-					onFocus={() => setSearchStyles(styles["search-active"])}
-					onBlur={() => setSearchStyles(styles.search)}
-				/>
+			<div className={styles.search}>
+				<input type="text" className={styles["search-input"]} />
 				<SearchIcon className={styles["search-icon"]} />
 			</div>
 
 			<div className={styles.links}>
-				<Link to="/login">Login</Link>
-				<Link to="/register">Register</Link>
-				<Link to="/checkout">
-					<ShoppingCartIcon />
-				</Link>
+				{isLoggedIn ? (
+					<>
+						<Link to="/profile">
+							<div className={styles.links}>
+								<AccountBoxIcon />
+								<span>Profile</span>
+							</div>
+						</Link>
+						<Link to="/purchases">
+							<div className={styles.links}>
+								<HistoryIcon />
+								<span>Purchases</span>
+							</div>
+						</Link>
+						<Link to="/checkout">
+							<div className={styles.links}>
+								<ShoppingCartIcon />
+								Cart
+							</div>
+						</Link>
+						<Link to="/" onClick={logout}>
+							<div className={styles.links}>
+								<ExitToAppIcon />
+								<span>Logout</span>
+							</div>
+						</Link>
+					</>
+				) : (
+					<>
+						<Link to="/login">Login</Link>
+						<Link to="/register">Register</Link>
+						<Link to="/checkout">
+							<div className={styles.links}>
+								<ShoppingCartIcon />
+								Cart
+							</div>
+						</Link>
+					</>
+				)}
 			</div>
 		</nav>
 	);
