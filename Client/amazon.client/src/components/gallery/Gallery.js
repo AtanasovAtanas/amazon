90df07e1d44901ea3ea4b8c7249cf86c9e79e702
Atanas Atanasov <atanas4537@gmail.com>
2020-09-12 18:53:49 +0300

    add Gallery component

---
+++ Client/amazon.client/src/components/gallery/Gallery.js
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from "react";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import styles from "./Gallery.module.css";
+import Lightbox from "../lightbox/Lightbox";
+
+const Gallery = ({ images }) => {
+	const [featured, setFeatured] = useState(images[0]);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const toggleModal = () => setIsModalOpen(!isModalOpen);
+
+	const sliderRef = useRef(null);
+
+	const onClickHandler = (event) => setFeatured(event.target.src);
+
+	const scrollLeft = () => {
+		sliderRef.current.scrollLeft -= 110;
+	};
+
+	const scrollRight = () => {
+		sliderRef.current.scrollLeft += 110;
+	};
+
+	return (
+		<>
+			<img
+				className={styles.featured}
+				src={featured}
+				onClick={toggleModal}
+			/>
+			<div className={styles.wrapper}>
+				<ChevronLeftIcon
+					className={styles.arrow}
+					fontSize="large"
+					onClick={scrollLeft}
+				/>
+				<div className={styles.slider} ref={sliderRef}>
+					{images.map((image, index) => {
+						const isActive = image === featured;
+						return (
+							<img
+								key={index}
+								className={
+									isActive ? styles.active : styles.picture
+								}
+								src={image}
+								onClick={(event) => onClickHandler(event)}
+							/>
+						);
+					})}
+				</div>
+				<ChevronRightIcon
+					className={styles.arrow}
+					fontSize="large"
+					onClick={scrollRight}
+				/>
+			</div>
+			{isModalOpen ? (
+				<Lightbox
+					featured={featured}
+					images={images}
+					closeHandler={toggleModal}
+				/>
+			) : null}
+		</>
+	);
+};
+
+export default Gallery;
