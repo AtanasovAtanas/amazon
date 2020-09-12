7b2e889e6ebc3ac9264958d5641b0da01339f741
Atanas Atanasov <atanas4537@gmail.com>
2020-09-12 16:15:28 +0300

    add lightbox component

---
+++ Client/amazon.client/src/components/lightbox/Lightbox.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styles from "./Lightbox.module.css";
+
+const Lightbox = ({ featured, images, closeHandler }) => {
+	const [main, setMain] = useState(featured);
+	const [index, setIndex] = useState(images.indexOf(featured));
+
+	const goToPrevImage = () => {
+		const nextIndex = index - 1 < 0 ? 0 : index - 1;
+		setIndex(nextIndex);
+		setMain(images[nextIndex]);
+	};
+
+	const goToNextImage = () => {
+		const nextIndex =
+			index + 1 >= images.length ? images.length - 1 : index + 1;
+
+		setIndex(nextIndex);
+		setMain(images[nextIndex]);
+	};
+
+	return (
+		<div className={styles.modal}>
+			<span className={styles.close} onClick={closeHandler}>
+				&times;
+			</span>
+			<div className={styles["modal-content"]}>
+				<div>
+					<img className={styles["modal-main-image"]} src={main} />
+				</div>
+
+				<a
+					className={`${styles.prev} ${
+						index === 0 ? styles.disabled : ""
+					}`}
+					onClick={goToPrevImage}
+				>
+					&#10094;
+				</a>
+				<a
+					className={`${styles.next} ${
+						index === images.length - 1 ? styles.disabled : ""
+					}`}
+					onClick={goToNextImage}
+				>
+					&#10095;
+				</a>
+
+				<div className={styles.row}>
+					{images.map((image, index) => (
+						<div key={index} className={styles.column}>
+							<img
+								className={
+									image === main ? styles.active : styles.demo
+								}
+								src={image}
+								alt="Nature"
+								onClick={() => {
+									setIndex(index);
+									setMain(image);
+								}}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Lightbox;
