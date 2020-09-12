9679ec9c7cd0c5da5997c9ac5f3ce80c1bf143f2
Atanas Atanasov <atanas4537@gmail.com>
2020-09-12 18:54:41 +0300

    edit Product.js

--- Client/amazon.client/src/components/product/Product.js
+++ Client/amazon.client/src/components/product/Product.js
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "./Product.module.css";
 import { Link } from "react-router-dom";
 
-const Product = ({ imgSrc, title, price }) => {
+const Product = ({ imgSrc, id, slug, title, price }) => {
+	const linkTo = `/products/${id}/${slug}`;
+
 	return (
-		<div class={styles.card}>
+		<div className={styles.card}>
 			<img src={imgSrc} alt="Product image" />
 			<h3>{title}</h3>
-			<p class={styles.price}>${price.toFixed(2)}</p>
-			<Link to="#">See product details</Link>
+			<p className={styles.price}>${price.toFixed(2)}</p>
+			<Link to={linkTo}>See product details</Link>
 		</div>
 	);
 };
